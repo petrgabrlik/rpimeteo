0ce70c8ed7ec1a25f3f252065186553cb6092fab
Petr Gabrlik <petrgabrlik@email.cz>
2017-04-03 13:36:06 +0000

    api read data from databeze

--- api.py
+++ api.py
@@ -3,6 +3,7 @@
 
 from flask import Flask, render_template, request
 import app as meteoapp
+import sqlite3
 
 
 app = Flask(__name__)
@@ -12,9 +13,15 @@ app = Flask(__name__)
 def index():
     '''
     '''
-    data = meteoapp.get_data()
-    return render_template('index.html', title='Home', temp=data['temp'], hum=data['hum'], time=data['time'])
-
+    # data = meteoapp.get_data()
+    # return render_template('index.html', title='Home', temp=data['temp'], hum=data['hum'], time=data['time'])
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    c = conn.cursor()
+    c.execute('SELECT * FROM meteo ORDER BY date DESC LIMIT 1')
+    r = c.fetchone()
+    return render_template('index.html', title='Home', temp=r['temp'], hum=r['hum'], time=r['date'])
+    c.close()
 
 if __name__ == '__main__':
     # app.run()
